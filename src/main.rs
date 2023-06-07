6ca37decfb20b7da92ca73a5c955e4527163f359
Karim <karim@parity.io>
2023-06-07 18:13:39 +0200

    creating Polars dataframe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::Read;
-use apache_avro::types::Record;
 use apache_avro::AvroSchema;
 use apache_avro::from_value;
 use apache_avro::Reader;
@@ -8,21 +6,32 @@ use std::path::Path;
 use schema::*;
 mod schema;
 
+use polars::prelude::*;
+
 fn main() {
     let path = Path::new("westend_westend_20230411_westend_blocks.avro");
     let file = File::open(&path).unwrap();
     let schema_trait = Block::get_schema();
     let reader = Reader::with_schema(&schema_trait, file).unwrap();
+
+    // Create an empty DataFrame
+    let mut df = DataFrame::default();
     
     for record in reader {
         match record {
             Ok(value) => {
-                let block = from_value::<Block>(&value);
-                println!("{:?}", block);
+                let block = from_value::<Block>(&value).unwrap();
+
+                let s = Series::new("block", vec![block.relay_chain]); 
+
+                df.with_column(s).unwrap();
+                
             }
             Err(e) => panic!("{:?}", e),
         }
 
+        // Print the DataFrame
+        println!("{:?}", df);
         
         break;
     }
